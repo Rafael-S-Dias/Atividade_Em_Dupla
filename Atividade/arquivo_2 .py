3ab8e8ee1d4c7772641b3f328f1856d1a33bd8d6
Ic4ro <161033248+Ic4ro@users.noreply.github.com>
2024-10-03 14:18:47 +0000

    Salvar

--- Atividade/arquivo_2 .py
+++ Atividade/arquivo_2 .py
@@ -1,1 +1,19 @@
 print("Icaro")
+
+print("continuando projeto.")
+
+# Resolução de conflito
+
+# git rebase
+
+# Apague as linhas indesejadas.
+
+# git add .
+
+# git commit -m "Mensagem"
+
+# git rebase --continue
+
+# git pull
+
+# git push 
